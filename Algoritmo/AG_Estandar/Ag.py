3d7b23874384c4ba2ad4e0912a97a7cdefc8ddd5
Jose Grimaldo <josemanuelgrimaldovalero@gmail.com>
2026-02-28 11:08:22 -0600

    commit primer funcionamient completo del algoritmo genetico estandar

--- Algoritmo/AG_Estandar/Ag.py
+++ Algoritmo/AG_Estandar/Ag.py
@@ -4,10 +4,13 @@ import pandas as pd
 import random
 from Algoritmo.Models.Celula import Celula
 
-poblacion_size = 100
 matriz_distancias = {}
+cantidad_generaciones = 10
+poblacion_size = 20
 porcentaje_elitismo = 0.1
 torneo_size = 5
+tasa_mutacion = 0.1
+ciculos_mutacion = 5
 
 
 def cargar_datos():
@@ -51,41 +54,99 @@ def generar_poblacion():
     return celulas
 
 
-def seleccionar_padres(celulas):
+def seleccionar_cruzar_mutar_evaluar(celulas):
     cantidad_elitismo = int(len(celulas) * porcentaje_elitismo)
-    elitaria = seleccion_elitaria(celulas, cantidad_elitismo)
-    torneo = seleccion_torneo(celulas, cantidad_elitismo)
-    return [elitaria, torneo]
+    celulas_elitistas = seleccion_elitista(celulas, cantidad_elitismo)
+    rutas_torneo = seleccion_torneo_cruza(celulas, cantidad_elitismo)
+
+    mutar(rutas_torneo)
+
+    nuevas_celulas_torneo = []
+    for ruta in rutas_torneo:
+        nueva = Celula(ruta)
+        nueva.fitness = evaluar_ruta(ruta)
+        nuevas_celulas_torneo.append(nueva)
+
+    return celulas_elitistas + nuevas_celulas_torneo
 
 
-def seleccion_elitaria(celulas, cantidad_elitismo):
-    sorted_celulas = sorted(celulas, key=lambda c: c.fitness)
-    return sorted_celulas[:cantidad_elitismo]
+def seleccion_elitista(celulas, cantidad_elitismo):
+    celulas_ordenadas = sorted(celulas, key=lambda c: c.fitness)
+    return celulas_ordenadas[:cantidad_elitismo]
 
 
-def seleccion_torneo(celulas, cantidad_elitismo):
-    padres = []
-    cantidad_torneo = len(celulas) // 2 - (cantidad_elitismo // 2)
+def seleccion_torneo_cruza(celulas, cantidad_elitismo):
+    resultado_cruza = []
+    cantidad_torneo = (len(celulas) - cantidad_elitismo) // 2
     for _ in range(cantidad_torneo):
-        torneo = random.sample(celulas, torneo_size)
-        ganador = min(torneo, key=lambda c: c.fitness)
-        padres.append(ganador)
-    return padres
+        torneo1 = random.sample(celulas, torneo_size)
+        padre1 = min(torneo1, key=lambda c: c.fitness)
+        torneo2 = random.sample(celulas, torneo_size)
+        padre2 = min(torneo2, key=lambda c: c.fitness)
+
+        hijo1, hijo2 = cruzar(padre1, padre2)
+        # print("padres")
+        # print(f"Ruta: {' -> '.join(padre1.ruta)} - Distancia: {padre1.fitness}")
+        # print(f"Ruta: {' -> '.join(padre2.ruta)} - Distancia: {padre2.fitness}")
+
+        # print("hijos")
+        # print(f"Ruta: {' -> '.join(hijo1)}")
+        # print(f"Ruta: {' -> '.join(hijo2)}")
+
+        resultado_cruza.append(hijo1)
+        resultado_cruza.append(hijo2)
+
+    return resultado_cruza
+
+
+def cruzar(padre1, padre2):
+    ruta1, ruta2 = padre1.ruta, padre2.ruta
+    size = len(ruta1)
+    punto1 = size // 3
+    punto2 = 2 * size // 3
+
+    def generar_hijo(p1, p2):
+        hijo = [None] * size
+
+        hijo[punto1:punto2] = p1[punto1:punto2]
+
+        nodos_restantes = [nodo for nodo in p2 if nodo not in hijo[punto1:punto2]]
+        for i in range(punto1):
+            hijo[i] = nodos_restantes.pop(0)
+
+        for i in range(punto2, size):
+            hijo[i] = nodos_restantes.pop(0)
+
+        return hijo
+
+    hijo1 = generar_hijo(ruta1, ruta2)
+    hijo2 = generar_hijo(ruta2, ruta1)
+
+    return hijo1, hijo2
+
+
+def mutar(rutas):
+
+    size = len(rutas[0])
+
+    for ruta in rutas:
+        if random.random() < tasa_mutacion:
+            for _ in range(ciculos_mutacion):
+                idx1, idx2 = random.sample(range(size), 2)
+                ruta[idx1], ruta[idx2] = ruta[idx2], ruta[idx1]
 
 
 def ejecutar_algoritmo():
     celulas = generar_poblacion()
-    elitismo, torneo = seleccionar_padres(celulas)
+    i = 0
+    while True:
+        i += 1
 
-    for celula in elitismo:
-        print(
-            f"RutaElitista: {' -> '.join(celula.ruta)} | Distancia Total: {celula.fitness}"
-        )
-
-    for celula in torneo:
-        print(
-            f"RutaTorneo: {' -> '.join(celula.ruta)} | Distancia Total: {celula.fitness}"
-        )
-
-    print("celulas", len(celulas))
-    print("PadresTorneo", len(torneo))
+        print(f"Generacion {i}")
+        timer = time()
+        celulas = seleccionar_cruzar_mutar_evaluar(celulas)
+        timer = time() - timer
+        print(f"Tiempo de generacion: {timer:.10f} segundos")
+        print("Promiedio de fitness: ", sum(c.fitness for c in celulas) / len(celulas))
+        for celula in celulas:
+            print(f"Ruta: {' -> '.join(celula.ruta)} - Distancia: {celula.fitness}")
